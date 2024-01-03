66b4e928b881ad55742dd73212e47a0b7843e1fd
Budidhavinaykumargoud <vinaygoud911@gmail.com>
2024-01-03 14:46:05 +0530

    PracticeProject

---
+++ Models/UserContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PracticeProject4.Models
+{
+    public class UserContext :DbContext
+    {
+        public UserContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Users> Users { get; set; }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PracticeProject4.Models
+{
+    public class Users
+    {
+        [Key]
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public string Marks { get; set; }
+
+
+    }
+
+}
